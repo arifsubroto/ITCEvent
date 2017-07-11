6632e40bf4c4f898e0fb3bc536ea808e0ed739d4
arifsubroto <arifsubroto8@gmail.com>
2017-07-11 21:34:23 +0700

    First Commit

---
+++ app/src/main/java/com/arifsubroto/itcevent/AdminHomeActivity.java
@@ -0,0 +1,23 @@
+package com.arifsubroto.itcevent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.arifsubroto.itcevent.R;
+
+/**
+ * Created by LabTIA on 11/07/2017.
+ */
+
+public class AdminHomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+
+    int a = 10;
+
+    String b = "dad";
+}
---
+++ app/src/main/java/com/arifsubroto/itcevent/Cek.java
@@ -0,0 +1,8 @@
+package com.arifsubroto.itcevent;
+
+/**
+ * Created by LabTIA on 11/07/2017.
+ */
+
+public class Cek {
+}
